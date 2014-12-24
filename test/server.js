f581a1d7c03ead29595d9c4e6b95ebcbd677fac8
Ross Zurowski <ross.zurowski@gmail.com>
2014-12-24 10:12:06 -0500

    Got some more stuff working

--- test/server.js
+++ test/server.js
@@ -2,17 +2,75 @@ var path    = require('path');
 var should  = require('should');
 var express = require('express');
 var request = require('supertest');
-var assets  = require('..');
+var Server  = require('..');
+
+// asset server requires
+var path = require('path');
+var map = require('map-stream');
+var check = require('gulp-if');
+
+var sass = require('gulp-sass');
+var autoprefix = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+
+var Duo = require('duo');
+var concat = require('gulp-concat');
+var es6 = require('gulp-6to5');
+var uglify = require('gulp-uglify');
+
+var env = process.env.NODE_ENV;
+
+// Asset server
+
+function assets(root) {
+
+	var srv = new Server(root)
+		// styles
+		// run on all scss files regardless of where they are in the path
+		.get('**/*.css')
+			.src('**/*.scss')
+			.use(sass())
+			.use(autoprefix())
+				.use(check('production' === env, csso()))
+		// // scripts
+		// run only on first-level javascript files
+		.get('libraries.js')
+			.src('js/libraries/*.js')
+			.use(concat('libraries.js'))
+		.get('index.js')
+			.use(duo({ root: root, components: './.dependencies' }))
+			.use(es6())
+				.use(check('production' === env, uglify()))
+
+	return srv.handler();
+
+}
+
+
+/**
+ * Prep duo for gulp streams
+ * @param {Object} opts
+ */
+function duo(opts) {
+	opts = opts || {};
+	var compiler = new Duo(opts.root);
+	if (opts.components) compiler.installTo(opts.components);
+	return map(function(file, done) {
+		compiler.entry(file.path)
+		.run(function(err, src) {
+			if (err) return fn(err);
+			file.contents = new Buffer(src);
+			done(null, file);
+		});
+	});
+}
 
 // Little sample application
 var app = express();
 app.use(assets(path.join(__dirname, '/assets')));
-app.get('*', function(req, res, next) {
-	res.status(404).send('Page not found');
-});
 app.use(function(err, req, res, next) {
 	var status = err.status || 500;
-	// console.log(err.message);
+	console.log('error', err.message, err.stack);
 	res.status(status).end();
 });
 // load it into supertest
@@ -31,10 +89,10 @@ describe('GET /path/to/asset', function() {
 			.end(done);
 	});
 	
-	it ('should 403 for directories', function(done) {
+	it ('should 404 for directories', function(done) {
 		request
 			.get('/css/')
-			.expect(403)
+			.expect(404)
 			.end(done);
 	});
 	
